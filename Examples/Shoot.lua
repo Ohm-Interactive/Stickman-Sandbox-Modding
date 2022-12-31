424328d92f4a16c8788f797ee386f62f5c770c55
Twingamerdudes <81382687+Twingamerdudes@users.noreply.github.com>
2022-12-31 11:32:50 -0500

    Removed uneeded line of code

--- Examples/Shoot.lua
+++ Examples/Shoot.lua
@@ -1,6 +1,5 @@
 function Start()
     print("Mod Started")
-    Sandbox.CreateItem(Sandbox.ModDirectory.."Example Mod/YouBastards.png", 312, 156)
     Sandbox.SpawnObject("Gun", 5, 0)
 end
 function Update(Keycodes)
